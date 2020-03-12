81cd2fd61e4fe8fe7067626c199fdc639f5f04e6
oliverochman <oliver.ochman@hotmail.com>
2020-03-12 10:58:49 +0100

    finish fb implementation

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
 import React from 'react';
+import { FacebookProvider, LoginButton } from "react-facebook";
+import axios from 'axios';
+
 
 const App = () => {
+
+  const handleResponse = async data => {
+    const response = await axios.post("http://localhost:3000/api/v1/auth", {
+      uid: data.profile.id,
+      email: data.profile.email,
+      provider: "facebook"
+    });
+    if (response.status === 200) {
+      debugger;
+    }
+  }
+
   return (
-    <>
+    <div>
       <h1>Slowfood</h1>
-    </>
+      <FacebookProvider appId="216164319761067">
+        <LoginButton scope="email" onCompleted={handleResponse}>
+        <span>Login via Facebook</span>
+        </LoginButton>
+      </FacebookProvider>
+    </div>
   );
 }
 
